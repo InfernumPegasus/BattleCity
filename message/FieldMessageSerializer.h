a6ccd8c92f7ab11f2807816eaffa67c2ec92c1a2
Vladimir Kirillov <vkirillov505@gmail.com>
2023-01-13 00:03:57 +0300

    added descriptions

--- message/FieldMessageSerializer.h
+++ message/FieldMessageSerializer.h
@@ -3,6 +3,7 @@
 
 #include "FieldMessage.h"
 
+// Serializer for FieldMessage class
 class FieldMessageSerializer {
 
 public:
@@ -13,9 +14,18 @@ public:
     FieldMessageSerializer() = delete;
 
 public:
+    /*
+     * Creates FieldMessage object's binary representation.
+     * Encoding format:
+     * [Message size (8b)] [Bit mask (8b)] [fields...].
+     * std::string field is encoded in such a format:
+     * [127 (1b)] [string.length() (2b)] [content]
+     * std::int32_t field is encoded in a such format:
+     * [0 (1b)] [content (4b)]
+     */
     static std::string Serialize(const FieldMessage &message);
 
-    static FieldMessage Deserialize(const std::string& serialized);
+    static FieldMessage Deserialize(std::string &serialized);
 };
 
 
